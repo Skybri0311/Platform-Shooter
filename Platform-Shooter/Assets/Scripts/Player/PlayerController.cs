ac8e88084e19f18080760bedac29e54cb9e179b1
Brian Fiedler <bfiedlerjr93@gmail.com>
2021-11-21 23:54:24 -0700

    Test Build 2

--- Platform-Shooter/Assets/Scripts/Player/PlayerController.cs
+++ Platform-Shooter/Assets/Scripts/Player/PlayerController.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //public fields
-    public float _speed = 1;
+    
 
     //private fields
+    [SerializeField] float _runSpeedModifier;
+    [SerializeField] Transform _groundCheckCollider;
+    [SerializeField] LayerMask _groundLayer;
+    [SerializeField] float _speed = 1;
+    [SerializeField] float _jumpPower = 300;
+
     Rigidbody2D _rb;
     Animator _animator;
+
+    const float _groundCheckRadius = 0.2f;
     float _horizontalValue;
-    [SerializeField] float _runSpeedModifier;
-    bool _isRunning = false;
+    bool _jump;
+    bool _isGrounded;
+    bool _isRunning;
     bool _facingRight = true;
 
     //Set rigidbody when game starts
@@ -33,19 +42,46 @@ public class PlayerController : MonoBehaviour
         //If Lshift is not pressed, disable _isRunning
         if (Input.GetKeyUp(KeyCode.LeftShift))
             _isRunning = false;
-       
+        //If player presses jump button, enable jump
+        if (Input.GetButtonDown("Jump"))
+            _jump = true;
+        //If player is not pressing jump button, disable jump
+        else if (Input.GetButtonUp("Jump"))
+            _jump = false;
     }
     
     //Call Move Method wiht the horizontal value
     void FixedUpdate()
     {
-        Move(_horizontalValue); 
+        GroundCheck();
+        Move(_horizontalValue, _jump); 
     }
-    //Change Velocity of rigid body
-    void Move(float dir)
+
+    void GroundCheck()
     {
+        _isGrounded = false;
+        //Check if the GroundCheckObject is colliding with the other 2D colliders thert are in the ground layer
+        //if yes (isGrounded true) else (isGrounded false)
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(_groundCheckCollider.position, _groundCheckRadius, _groundLayer);
+        if (colliders.Length > 0)
+            _isGrounded = true;
+
+      
+    }
+
+    
+    void Move(float dir,bool _jumpFlag)
+    {
+        if(_isGrounded && _jumpFlag)
+        {
+            _isGrounded = false;
+            _jumpFlag = false;
+            _rb.AddForce(new Vector2(0f, _jumpPower));
+        }
+
+        #region Move & Run
         //Set value of x using dir and speed
-        float xVal = dir * _speed* 100 * Time.deltaTime;
+        float xVal = dir * _speed* 100 * Time.fixedDeltaTime;
         //If player is running, multiply by Modifier value
         if (_isRunning)
             xVal *= _runSpeedModifier;
@@ -70,5 +106,6 @@ public class PlayerController : MonoBehaviour
         //(0 Idle , 6 walking , 12 running)
         //Set the float xVelocity according to the x value of the RigidBody2D velocity
         _animator.SetFloat("xVelocity", Mathf.Abs(_rb.velocity.x));
+        #endregion
     }
 }
